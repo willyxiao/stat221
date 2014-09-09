0dfe38ba801c89c1da7b44f4239a0a31e47b2723
Kevin Eskici <keskici@college.harvard.edu>
2014-09-09 18:38:12 -0400

    wrote dlogisticnorm

--- pset1_posted/keskici_wxiao_ps1_prob1.R
+++ pset1_posted/keskici_wxiao_ps1_prob1.R
@@ -12,5 +12,17 @@ covariance_matrix = function(d, alpha, beta){
   
 
 dlogisticnorm = function(u, mu, alpha, beta){
-  2*pi*covariance_matrix
+  cov_mtrx = covariance_matrix(length(u),alpha, beta)
+  
+  if(sum(u) >= 1 | length(u) != length(mu)){
+    return (0) #do we want an error instead?
+  }
+  
+  u_d_plus_one = 1 - sum(u)
+  
+  a = abs(det(2*pi*cov_mtrx))^-.5
+  b = 1/(prod(u)*(u_d_plus_one))
+  c = t(log(u/u_d_plus_one) - mu) %*% (1/cov_mtrx) %*% (log(u/u_d_plus_one) - mu)
+  return (a*b*exp(-.5*c))
 }
+
