3a837c28a3fc00ca4efb5591199b75186b67c330
Kevin Eskici <keskici@college.harvard.edu>
2014-09-09 12:39:59 -0400

    Moved in pset1 folder

--- pset1_posted/keskici_wxiao_ps1_prob1.R
+++ pset1_posted/keskici_wxiao_ps1_prob1.R
@@ -4,15 +4,13 @@ data = read.table("dataLogisticNorm3D.txt", header = TRUE)
 covariance_matrix = function(d, alpha, beta){
   entries = rep(-beta, d*d)
   m = matrix(entries,nrow = d, ncol = d)
-  for(i in 1:d)
-  {
+  for(i in 1:d) {
     m[i,i] = alpha
   }
-  
   return(m)
 }
   
 
 dlogisticnorm = function(u, mu, alpha, beta){
-  
+  2*pi*covariance_matrix
 }
