9d76772b8b6854efd0c2e435c6fef51e99ed1c8e
Kevin Eskici <keskici@college.harvard.edu>
2014-09-16 22:17:17 -0400

    Created Problem2 R file

---
+++ pset1_posted/keskici_wxiao_ps1_prob2.R
@@ -0,0 +1,17 @@
+
+
+#2.2
+ll = function(G, theta, X){
+  
+  
+}
+
+
+data_area2 = read.table('data1985_area2.csv', header=T)
+
+load('theta0list.Rdata') #Note: variable name is theta0List
+
+#2.3
+gomMLE = function(X, GO, thetaO){
+  
+}
