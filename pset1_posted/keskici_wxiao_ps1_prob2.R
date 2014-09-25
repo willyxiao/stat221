e8d9426e1507bd7660e810e40fceb2b79b82b738
Willy Xiao <wxiao@hcs.harvard.edu>
2014-09-24 23:41:02 -0400

    more renaming

--- pset1_posted/keskici_wxiao_ps1_prob2.R
+++ pset1_posted/keskici_wxiao_ps1_prob2.R
@@ -69,85 +69,68 @@ logistic <- function(v){
 }
 
 # computes the log likelihood but replaces an NA or -Inf with BIG.NEGATIVE
-optim.G <- function(G, Theta, X){
+optim.G <- function(G, theta, X){
   lik = ll(G, theta, data)
   ifelse(lik == -Inf || is.na(lik), BIG.NEGATIVE, lik)
 }
 
-# computes the log-likelihood over a single feature j
-ll.theta <- function(G, theta_vec, Xj){
-  k.j <- Xj + 1
-  sum(log(G*theta_vec[k.j]))
-}
-
-
-llTheta.check <- function(){
-  x <- c(0,1,2)
-  theta_Lj <- c(.2,.7,.1)
-  theta_Hj <- c(.1,.5,.4)
-  G <- c(.1,.5,.2)
-
-  #.1 * (.2) + .9 * (.1) = .02 + .09 = .11
-  #.5 * (.7) + .5 * (.5) = .35 + .25 = .60
-  #.2 * (.1) + .8 * (.4) = .02 + .32 = .34
-  checkTrue(round(llTheta(G, theta_Lj, theta_Hj, x), 4) == -3.7969)
-}
 # G is a N x 1 vector where G[1] is G_Li
 # X is a N x J matrix where X[n,j] is the category of plot i feature j
-optim.theta <- function(theta_L, theta_H, j, G, theta, X, high_flag){
-  #ld_thetas <- theta_L
+optim.theta <- function(theta.L, theta.H, j, G, theta, X, high_flag){
   if(high_flag){
-    #theta_H <- logistic(theta_H)
-    return(ll.theta((1-G), logistic(theta_H), X[,j]))
+    ll.theta((1-G), logistic(theta.H), X[,j])
   } else {
-    #theta_L <- logistic(theta_L)
-    return(ll.theta(G, logistic(theta_L), X[,j]))
+    ll.theta(G, logistic(theta.L), X[,j])
   }
+}
 
-#  stopifnot(!all(theta_H > 0 & theta_L > 0))
-  #if(!all(theta_H > 0) || !all(theta_L > 0)){
-  #  browser()
-  #  return (BIG.NEGATIVE)
-  #} 
-  
-  #llTheta(G, theta_L, theta_H, X[,j])
+# computes the log-likelihood over a single feature j as a high plot or a low-plot
+ll.theta <- function(G, theta.vec, Xj){
+  k.j <- Xj + 1
+  sum(log(G*theta.vec[k.j]))
 }
 
 #2.3
 # OPTIMIZE (gomMLE takes like wayyy too long to run)
 gomMLE <- function(X, G0, theta0){
-  lik <- -Inf
-  lik1 <- 0
-  
   G = G0
   theta <- theta0
+
+  lik = -Inf
+  lik1 = 0
+  MLES = NA
   
-  N <- length(X)
-  J <- length(theta)
-  
+  J = length(theta)
+    
   while(lik != lik1) {
     
-    # g_L,n for n = 1,...,N
+    # OPTIMIZE G
     loginfo("Running G...")
     print(G)
-    res = optim(par=G, fn=optim.G, method="L-BFGS-B", X=X, Theta=theta, control=list(fnscale=-1))
+
+    res = optim(par=G, 
+                fn=optim.G, 
+                method="L-BFGS-B", 
+                X=X, theta=theta, 
+                control=list(fnscale=-1))
+    
     loginfo("Distance: %f", sum((G - res$par)**2))
-    G <- res$par    
+    G = res$par    
     loginfo("Result: %f", res$value)
     print(G)
     
+    # OPTIMIZE THETA LOWS
     loginfo("Running lows...")
-    # theta_l,j for j = 1,...,J
     for(j in 1:J){
-      theta_j <- theta[[j]]
-      theta_Lj <- theta_j$low
-      theta_Hj <- theta_j$high
+      theta.j <- theta[[j]]
+      theta.Lj <- theta.j$low
+      theta.Hj <- theta.j$high
 
-      res = optim(par=rep(.001, length(theta_Lj)), 
+      res = optim(par=rep(.001, length(theta.Lj)), 
                   fn=optim.theta,
                   method="L-BFGS-B",
-                  X=X, theta_H=theta_Hj, G=G, j=j, high_flag = FALSE, 
-                  lower = rep(-10,length(theta_Lj)), upper = rep(10, length(theta_Lj)),
+                  X=X, theta.H=theta.Hj, G=G, j=j, high_flag = FALSE, 
+                  lower = rep(-10,length(theta.Lj)), upper = rep(10, length(theta.Lj)),
                   control=list(fnscale=-1))
       dummy = logistic(res$par)
       theta[[j]]$low <- dummy
@@ -155,50 +138,44 @@ gomMLE <- function(X, G0, theta0){
       loginfo("On feature: %2d, loglik: %f", j, ll(G, theta, X))
     }
 
+    # OPTIMIZE THETA HIGHS
     loginfo("Running highs")
     for(j in 1:J){
       theta_j <- theta[[j]]
-      theta_Lj <- theta_j$low
-      theta_Hj <- theta_j$high 
-      old_theta_Hj <- theta_Hj
+      theta.Lj <- theta_j$low
+      theta.Hj <- theta_j$high 
+      old_theta.Hj <- theta.Hj
       old_val <- ll(G, theta, X)
-#      old_val <- lloptim.theta2check(theta_L=theta_Lj,theta_H=theta_Hj,j=j,G=G,X=X)
+#      old_val <- lloptim.theta2check(theta.L=theta.Lj,theta.H=theta.Hj,j=j,G=G,X=X)
       #old_theta <- unlist(theta)
-      res <- optim(par=rep(.001, length(theta_Hj)), 
+      res <- optim(par=rep(.001, length(theta.Hj)), 
                   fn=optim.theta,
                   method="L-BFGS-B",
-                  X=X, theta_L=theta_Lj, G=G, j=j, high_flag = TRUE, 
-                  lower = rep(-10,length(theta_Hj)), upper = rep(10, length(theta_Hj)),
+                  X=X, theta.L=theta.Lj, G=G, j=j, high_flag = TRUE, 
+                  lower = rep(-10,length(theta.Hj)), upper = rep(10, length(theta.Hj)),
                   control=list(fnscale=-1))
       dummy <- logistic(res$par)
       
       theta[[j]]$high <- dummy
-      #if(ll(G, theta, X) < old_val){
-#        print(old_theta - unlist(theta))
-#        print(theta[[j]]$high)
-#        print(old_theta_Hj)
-        #print(res$par)
-      #  browser()
-      #}
-                  
+
       loginfo("On feature: %2d, loglik: %f", j, ll(G, theta, X))
       lik_holder <- ll(G, theta, X)
     }
     lik1 <- lik
     lik <- lik_holder    
     loginfo("Old: %f, New: %f", lik1,lik)
-    
-    MLES <<- list(G.hat=G, theta.hat=theta, maxlik=lik)
+
+        
+    MLES = list(G.hat=G, theta.hat=theta, maxlik=lik)
   }
 
-  return (MLES)
+  MLES
 }
 
+# to run our gomMLE with defaults
 run.gomMLE <- function(X=data,G0=FALSE,theta0=theta0List) {
   if(!G0){
-    
     G0 <- rbeta(dim(X)[1],100,100)
-    #G0 <- rep(.5,dim(X)[1])
   }
-  return (gomMLE(X,G0,theta0))  
+  gomMLE(X,G0,theta0)
 }
