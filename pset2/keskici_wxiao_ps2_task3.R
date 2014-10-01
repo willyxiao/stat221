c843efecc99875b9f8eafa662ac2033eccc68bcc
Willy Xiao <wxiao@college.harvard.edu>
2014-10-01 16:58:03 -0400

    generating Y

--- pset2/keskici_wxiao_ps2_task3.R
+++ pset2/keskici_wxiao_ps2_task3.R
@@ -8,9 +8,14 @@ w           <- rep(1, J) # weights are all set to 1
 mu          <- c(1.6, 2.5, 5.2, 4.9)
 sigma       <- c(0.7, 1.3, 1.3, 1.6)
 
+object.name.str <- "Y.%d.%d"
+file.name.str <- "Y/Y_%d_%d"
+
 for(i in 1:length(mu)){
   for(j in 1:theta.NSIMS) {
-    log.theta = simThetagivenMuSigma(mu[i], sigma[i], J)  
+    log.theta = simThetagivenMuSigma(mu[i], sigma[i], J)
     Y = simYgivenTheta(exp(log.theta), w, N)
+    assign(sprintf(object.name.str, i, j), Y)
+    save(list=c(sprintf(object.name.str, i, j)), file=sprintf(file.name.str, i, j))
   }
 }
