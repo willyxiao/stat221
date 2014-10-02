668f90bf159f864c63b1c2f942d696e4cd9ee9a0
Willy Xiao <wxiao@college.harvard.edu>
2014-10-02 09:27:00 -0400

    change functions

--- pset2/keskici_wxiao_ps2_functions.R
+++ pset2/keskici_wxiao_ps2_functions.R
@@ -1,5 +1,10 @@
+N           <- 2    # number of draws
+J           <- 1000 # length of theta and w vector
+theta.NSIMS <- 2    # theta.nsims * N is the # of total simulations we'll run
 
-
+w           <- rep(1, J) # weights are all set to 1
+mu          <- c(1.6, 2.5, 5.2, 4.9)
+sigma       <- c(0.7, 1.3, 1.3, 1.6)
 
 # theta is length J
 # w is length J
@@ -13,5 +18,18 @@ simYgivenTheta <- function(theta, w, N) {
 # all scalars
 simThetagivenMuSigma <- function(mu, sigma, J) {
   rnorm(J, mu, sigma)
+}
+
+# returns the object to be saved to *.RData given the i'th mu/sigma pair 
+# and the j'th simulated theta
+getObjectName <- function(i, j){
+  object.name.str <- "Y.%d.%d"
+  sprintf(object.name.str, i, j)
+}
+
+# same as getObjectName, but gets the file name
+getFileName <- function(i,j){
+  file.name.str <- "Y/Y_%d_%d.RData"
+  sprintf(file.name.str, i, j)
 }
 
--- pset2/keskici_wxiao_ps2_task3.R
+++ pset2/keskici_wxiao_ps2_task3.R
@@ -1,21 +1,10 @@
 source("keskici_wxiao_ps2_functions.R")
 
-N           <- 2    # number of draws
-J           <- 1000 # length of theta and w vector
-theta.NSIMS <- 2    # theta.nsims * N is the # of total simulations we'll run
-
-w           <- rep(1, J) # weights are all set to 1
-mu          <- c(1.6, 2.5, 5.2, 4.9)
-sigma       <- c(0.7, 1.3, 1.3, 1.6)
-
-object.name.str <- "Y.%d.%d"
-file.name.str   <- "Y/Y_%d_%d"
-
 for(i in 1:length(mu)){
   for(j in 1:theta.NSIMS) {
     log.theta = simThetagivenMuSigma(mu[i], sigma[i], J)
     Y = simYgivenTheta(exp(log.theta), w, N)
-    assign(sprintf(object.name.str, i, j), Y)
-    save(list=c(sprintf(object.name.str, i, j)), file=sprintf(file.name.str, i, j))
+    assign(getObjectName(i,j), Y)
+    save(list=c(getObjectName(i,j)), file=getFileName(i,j))
   }
 }
