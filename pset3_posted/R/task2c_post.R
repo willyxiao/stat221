76080e277c2c49e66f2838aba2e00a90403aec85
Willy Xiao <willy@chenxiao.us>
2014-10-22 16:17:52 -0400

    fix R

--- pset3_posted/R/task2c_post.R
+++ pset3_posted/R/task2c_post.R
@@ -1,15 +1,15 @@
 source('task2c_runner.R')
 
 for(alg.name in c('sgd', 'asgd', 'implicit')){
-  plot.name = paste(alg.name, toString(theta.run), "var", "plot", sep="_")
+  plot.name = paste(alg.name, toString(theta.run), "plot", sep="_")
   print(plot.name)
   pdf(paste(plot.name, "pdf", sep="."))
   load(file.name(alg.name, 1))
-  plot(nlist, log10(res$dist), "l", ylim=c(-1.25, -.5), lwd=2, col=colors()[25], ann=FALSE)
+  plot(nlist, log10(res$bias), "l", lwd=2, col=colors()[25], ann=FALSE)
   
   for(a.id in 2:length(a.tests)){
     load(file.name(alg.name, a.id))
-    lines(nlist, log10(res$dist), lwd=2, col=colors()[(a.id-1)*25])
+    lines(nlist, log10(res$bias), lwd=2, col=colors()[(a.id-1)*25])
   }
 
   title(main=plot.name, 
