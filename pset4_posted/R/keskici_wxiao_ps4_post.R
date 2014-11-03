4f00356a719ea6d54deb5fe9a883ee24b56d6e43
Willy Xiao <wxiao@college.harvard.edu>
2014-11-02 23:29:36 -0500

    post processing working

--- pset4_posted/R/keskici_wxiao_ps4_post.R
+++ pset4_posted/R/keskici_wxiao_ps4_post.R
@@ -5,23 +5,25 @@ log.posterior.analytic = function(N, Y){
   n = length(Y)
   
   numerator = sum(log(seq(1,S))) + sum(log(seq(1, n*N-S)))
-  denominator = sum(log(seq(1,n*N+1))) + N
+  denominator = sum(log(seq(1,n*N+1))) + log(N)
   
   (numerator - denominator) + sum(log(vapply(Y, choose, 1, n=N)))
 }
 
-optim.fun = function(norm.const, log.analytic, log.real){
-  sum((log.analytic + log(norm.const) - log.real)^2)
+optim.fun = function(log.norm.const, log.analytic, log.real){
+  stopifnot(length(log.analytic) == length(log.real))
+  stopifnot(length(log.norm.const) == 1)
+  sum((log.analytic + log.norm.const - log.real)^2)
 }
 
 process.data = function(num.jobs, run.n){
     tt = NA
     
-    for(i in 2:10){
+    for(i in 11:20){
       load(sprintf("keskici_wxiao_ps4_run%d_job%d.RData", run.n, i))
       r = range(chain[,1])
       tmp = table(chain[,1])
-      if(is.na(tt)){
+      if(length(tt) == 1 && is.na(tt)){
         tt = tmp
       } else{
         tt = c(tt, tmp)
@@ -35,8 +37,19 @@ process.data = function(num.jobs, run.n){
     tt
 }
 
-res = process.data(NUM_JOBS, RUN_NUMBER)
-res = prop.table(res[vapply(sort(as.numeric(names(res))), toString, "hi")])
-barplot(res)
-tmp = vapply(as.numeric(names(res)), log.posterior.analytic, .5, Y=impala)
-optim(10, optim.fun, log.analytic=tmp, log.real=log(res), method="Brent", lower=0, upper=1e5)
+CUTOFF = 400
+
+run.post = function(){
+  res = process.data(NUM_JOBS, RUN_NUMBER)
+  res = prop.table(res[vapply(sort(as.numeric(names(res))), toString, "hi")])
+  barplot(res)
+  res
+}
+
+run.normalizing = function(res, y){
+  tmp = vapply(as.numeric(names(res[1:CUTOFF])), log.posterior.analytic, .5, Y=y)
+  barplot(tmp)
+  optim(20, optim.fun, log.analytic=tmp, log.real=log(res[1:CUTOFF]), method="Brent", lower=0, upper=20)  
+}
+
+
