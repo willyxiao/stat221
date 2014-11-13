6f07dcb3215b69d234f4f2091ca08bd69e3fbf73
Willy Xiao <wxiao@college.harvard.edu>
2014-11-13 15:11:24 -0500

    move Q

--- pset5_posted/R/keskici_wxiao_functions.R
+++ pset5_posted/R/keskici_wxiao_functions.R
@@ -23,32 +23,33 @@ locally_iid_EM.each = function(data, c, A){
   theta.k = NULL
   theta.k1 = c(lambda0, phi0)
   while(is.null(theta.k) || any(theta.k != theta.k1)){
-    Q = function(theta, theta.k){
-#       theta = exp(theta)
-      sigma = theta[x.length + 1]*diag(theta[1:x.length]^c)
-      sigma.k = theta.k[x.length + 1]*diag(theta.k[1:x.length]^c)
-      big.multiple = t(A)%*%qr.solve(A%*%sigma.k%*%t(A))
-      r.k = sigma.k - (sigma.k%*%big.multiple%*%A)%*%sigma.k
-      applied.sum = sum(apply(data, 1, function(row){
-        m = theta.k[1:x.length] + sigma.k%*%big.multiple%*%(row - A%*%theta.k[1:x.length])
-        t(m - theta[1:x.length])%*%sigma%*%(m - theta[1:x.length])
-      }))
-      res = -(length(data)/2)*(log(det(sigma)) + tr(qr.solve(sigma, r.k))) - (1/2)*applied.sum
-      if(res == Inf){
-        1e30
-      } else if (res == -Inf){
-        -1e30
-      } else{
-        res
-      }
-    }
-
     theta.k = theta.k1
-    theta.k1 = optim(theta.k, Q, theta.k=theta.k, method="L-BFGS-B", lower=rep(1e-6, length(theta.k)))$par
+    theta.k1 = optim(theta.k, Q, c=c, A=A, theta.k=theta.k)$par #, method="L-BFGS-B", lower=rep(1e-6, length(theta.k)))$par
     print(sum((theta.k1 - theta.k)^2))
   }
   
   theta.k
+}
+
+Q = function(theta, theta.k, c, A){
+  sigma = theta[x.length + 1]*diag(theta[1:x.length]^c)
+  sigma.k = theta.k[x.length + 1]*diag(theta.k[1:x.length]^c)
+  sigma.inverse = qr.solve(sigma)
+  big.multiple = t(A)%*%qr.solve(A%*%sigma.k%*%t(A))
+  r.k = sigma.k - (sigma.k%*%big.multiple%*%A)%*%sigma.k
+  applied.sum = sum(apply(data, 1, function(row){
+    m = theta.k[1:x.length] + sigma.k%*%big.multiple%*%(row - A%*%theta.k[1:x.length])
+    t(m - theta[1:x.length])%*%sigma.inverse%*%(m - theta[1:x.length])
+  }))
+  res = -(length(data)/2)*(log(det(sigma)) + tr(sigma.inverse%*%r.k)) - (1/2)*applied.sum
+#   if(res == Inf){
+#     1e30
+#   } else if (res == -Inf){
+#     -1e30
+#   } else{
+#     res
+#   }
+  res
 }
 
 smoothed_EM <- function(data, c, A){
