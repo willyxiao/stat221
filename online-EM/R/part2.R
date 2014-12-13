fc6e2aea94f7fac89cfcb649cf4773178d2ae928
Willy Xiao <wxiao@college.harvard.edu>
2014-12-13 11:28:21 -0500

    debuggin

--- online-EM/R/part2.R
+++ online-EM/R/part2.R
@@ -33,17 +33,20 @@ EPSILON = .01
 
 online.EM = function(lr.fun, data, start.avg=50){ # learning rate...    
 #   tmp = runif(1,0,1)
-
+  tmp = .5
   
   s.1 = list(tmp,1 - tmp)
   s.2 = list(0, 0)
   s.3 = list(0, 0)
   s.4 = list(0, 0)
 
+  A = list(0, 0)
+
+  A[[1]] = matrix(runif(16, -10, 10), nrow=4)
+  A[[2]] = A[[1]] + matrix(rnorm(16, 0, .5), nrow=4)
+
   for(j in 1:2){
-    A = matrix(runif(16, -10, 10), nrow=4)
-    pos.def = t(A)%*%A
-    
+    pos.def = t(A[[j]])%*%A[[j]]
     s.2[[j]] = pos.def[1:3, 4]
     s.3[[j]] = pos.def[1:3, 1:3]
     s.4[[j]] = pos.def[4,4]
@@ -70,13 +73,14 @@ online.EM = function(lr.fun, data, start.avg=50){ # learning rate...
                      *((r - t(beta[j,])%*%z)^2)
                      /sigma.sq[j]))
     }
+
     w[1] = tmp[1] / sum(tmp)
     w[1] = min(1 - EPSILON, max(EPSILON, w[1]))
     w[2] = 1 - w[1]
     
     lr.rate = lr.fun(i)
     
-    for(j in 1:2) { # only 2 classes
+    for(j in 1:2) {
       s.1[[j]] = s.1[[j]] + lr.rate*(w[j] - s.1[[j]])
       s.2[[j]] = s.2[[j]] + lr.rate*(w[j]*r*z - s.2[[j]])
       s.3[[j]] = s.3[[j]] + lr.rate*(w[j]*r*z%*%t(z) - s.3[[j]])
@@ -85,7 +89,6 @@ online.EM = function(lr.fun, data, start.avg=50){ # learning rate...
       beta[j,] = solve(s.3[[j]])%*%s.2[[j]]
       sigma.sq[j] = (s.4[[j]] - t(beta[j,])%*%s.2[[j]])/s.1[[j]]
     }
-
   }
       
   list(w=w, beta=beta, sigma.sq=sigma.sq)
