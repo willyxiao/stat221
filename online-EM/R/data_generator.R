d9db10ec21a2127aa06e9471e7d62147a5ce3de5
Kevin Eskici <keskici@college.harvard.edu>
2014-12-14 15:41:56 -0500

    added window means function

--- online-EM/R/data_generator.R
+++ online-EM/R/data_generator.R
@@ -5,12 +5,12 @@ dat = read.csv("1router_allcount.dat")
 src.fddi = dat[,3][17]
 src.switch = dat[,3][18]
 src.local = dat[,3][19]
-src.copr = dat[,3][20]
+src.corp = dat[,3][20]
 
 dst.fddi = dat[,3][21]
 dst.switch = dat[,3][22]
 dst.local = dat[,3][23]
-dst.copr = dat[,3][24]
+dst.corp = dat[,3][24]
 
 fddi.fddi = dat[,3][1]
 fddi.switch = dat[,3][2]
@@ -19,11 +19,11 @@ fddi.corp = dat[,3][4]
 switch.fddi = dat[,3][5]
 switch.switch = dat[,3][6]
 switch.local = dat[,3][7]
-switch.copr = dat[,3][8]
+switch.corp = dat[,3][8]
 local.fddi = dat[,3][9]
 local.switch = dat[,3][10]
 local.local = dat[,3][11]
-local.copr = dat[,3][12]
+local.corp = dat[,3][12]
 corp.fddi = dat[,3][13]
 corp.switch = dat[,3][14]
 corp.local = dat[,3][15]
@@ -33,11 +33,11 @@ corp.corp = dat[,3][16]
 x1 = (dat[dat[,3] == src.fddi,])[,'value']
 x2 = (dat[dat[,3] == src.switch,])[,'value']
 x3 = (dat[dat[,3] == src.local,])[,'value']
-x4 = (dat[dat[,3] == src.copr,])[,'value']
+x4 = (dat[dat[,3] == src.corp,])[,'value']
 x5 = (dat[dat[,3] == dst.fddi,])[,'value']
 x6 = (dat[dat[,3] == dst.switch,])[,'value']
 x7 = (dat[dat[,3] == dst.local,])[,'value']
-x8 = (dat[dat[,3] == dst.copr,])[,'value']
+x8 = (dat[dat[,3] == dst.corp,])[,'value']
 
 y1 = (dat[dat[,3] == fddi.fddi,])[,'value']
 y2 = (dat[dat[,3] == fddi.switch,])[,'value']
@@ -46,11 +46,11 @@ y4 = (dat[dat[,3] == fddi.corp,])[,'value']
 y5 = (dat[dat[,3] == switch.fddi,])[,'value']
 y6 = (dat[dat[,3] == switch.switch,])[,'value']
 y7 = (dat[dat[,3] == switch.local,])[,'value']
-y8 = (dat[dat[,3] == switch.copr,])[,'value']
+y8 = (dat[dat[,3] == switch.corp,])[,'value']
 y9 = (dat[dat[,3] == local.fddi,])[,'value']
 y10 = (dat[dat[,3] == local.switch,])[,'value']
 y11 = (dat[dat[,3] == local.local,])[,'value']
-y12 = (dat[dat[,3] == local.copr,])[,'value']
+y12 = (dat[dat[,3] == local.corp,])[,'value']
 y13 = (dat[dat[,3] == corp.fddi,])[,'value']
 y14 = (dat[dat[,3] == corp.switch,])[,'value']
 y15 = (dat[dat[,3] == corp.local,])[,'value']
@@ -63,6 +63,21 @@ split.dat = function(router.dat){
   means = colMeans(dat.matrix)
   vars = apply(dat.matrix, 2, var)
   return(list(means = means, vars = vars))
+}
+
+actual.window.means = function(iter=0){
+  means = split.dat(y1)$mean
+  for(i in 2:16){
+    y = get(sprintf("y%d", i))
+    res = split.dat(y)$means
+    means = cbind(means, res)
+  }
+  if (iter == 0){
+    means
+  }
+  else{
+    means[iter,]
+  }
 }
 
 #takes in generated router data (i.e. fddi->fddi)
