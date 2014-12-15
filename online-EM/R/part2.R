65559fed40c4732d4192572d9c9672c170cea457
Willy Xiao <wxiao@college.harvard.edu>
2014-12-15 10:24:34 -0500

    yaaay

--- online-EM/R/part2.R
+++ online-EM/R/part2.R
@@ -1,3 +1,5 @@
+rm(list=ls())
+
 library(matrixcalc)
 
 simulate.data = function(nsamples){
@@ -21,10 +23,9 @@ simulate.data = function(nsamples){
   list(data=data, class=class, U=Us)
 }
 
-EPSILON = .01
+INHIBITION = 20
 
-online.EM = function(lr.fun, data, start.avg=50){ # learning rate...    
-#   tmp = runif(1,0,1)
+online.EM = function(lr.fun, data, start.avg=50){
   tmp = .5
   
   s.1 = list(tmp,1 - tmp)
@@ -34,8 +35,8 @@ online.EM = function(lr.fun, data, start.avg=50){ # learning rate...
 
   A = list(0, 0)
 
-  A[[1]] = matrix(runif(16, 0, 40), nrow=4)
-  A[[2]] = A[[1]] + matrix(rnorm(16, 0, 5), nrow=4)
+  A[[1]] = matrix(runif(16, 0, 3), nrow=4)
+  A[[2]] = matrix(runif(16, 0, 3), nrow=4)
 
   for(j in 1:2){
     pos.def = t(A[[j]])%*%A[[j]]
@@ -43,18 +44,18 @@ online.EM = function(lr.fun, data, start.avg=50){ # learning rate...
     s.3[[j]] = pos.def[1:3, 1:3]
     s.4[[j]] = pos.def[4,4]
   }
-  
-  w = rep(0,2)
-  beta = matrix(0,nrow=2,ncol=3)
-  sigma.sq = rep(0,2)
-  
-  for(j in 1:2){
-    w[j] = s.1[[j]]
-    beta[j,] = solve(s.3[[j]])%*%s.2[[j]]
-    sigma.sq[j] = (s.4[[j]] - t(beta[j,])%*%s.2[[j]])/s.1[[j]]    
-  }
 
-  for(i in 1:length(data$data)){    
+  w = rep(.5,2)
+  beta = rbind(c(0, 4, 0), c(10, 10, 10))
+  sigma.sq = rep(200,2)
+  
+#   for(j in 1:2){
+#     w[j] = s.1[[j]]
+#     beta[j,] = solve(s.3[[j]])%*%s.2[[j]]
+#     sigma.sq[j] = (s.4[[j]] - t(beta[j,])%*%s.2[[j]])/s.1[[j]]
+#   }
+
+  for(i in 1:length(data$data)){  
     r = data$data[i]
     z = c(1, data$U[i], (data$U[i]^2)/10)
     
@@ -65,24 +66,26 @@ online.EM = function(lr.fun, data, start.avg=50){ # learning rate...
                      *((r - t(beta[j,])%*%z)^2)
                      /sigma.sq[j]))
     }
-
+    
     w[1] = tmp[1] / sum(tmp)
     w[2] = 1 - w[1]
     
     lr.rate = lr.fun(i)
     
     for(j in 1:2) {
-      s.1[[j]] = s.1[[j]] + lr.rate*(w[j] - s.1[[j]])
-      s.2[[j]] = s.2[[j]] + lr.rate*(w[j]*r*z - s.2[[j]])
-      s.3[[j]] = s.3[[j]] + lr.rate*(w[j]*r*z%*%t(z) - s.3[[j]])
-      s.4[[j]] = s.4[[j]] + lr.rate*(w[j]*r^2 - s.4[[j]])
+      s.1[[j]] = (1 - lr.rate)*s.1[[j]] + lr.rate*w[j]
+      s.2[[j]] = (1 - lr.rate)*s.2[[j]] + lr.rate*(w[j]*r*z)
+      s.3[[j]] = (1 - lr.rate)*s.3[[j]] + lr.rate*(w[j]*z%*%t(z))
+      s.4[[j]] = (1 - lr.rate)*s.4[[j]] + lr.rate*(w[j]*r^2)
 
-      w[j] = s.1[[j]]
-      beta[j,] = solve(s.3[[j]])%*%s.2[[j]]
-      sigma.sq[j] = (s.4[[j]] - t(beta[j,])%*%s.2[[j]])/s.1[[j]]
+      if(i >= INHIBITION) {
+        w[j] = s.1[[j]]
+        beta[j,] = solve(s.3[[j]])%*%s.2[[j]]
+        sigma.sq[j] = (s.4[[j]] - t(beta[j,])%*%s.2[[j]])/s.1[[j]]
+      }
     }
   }
-  
+
   list(w=w, beta=beta, sigma.sq=sigma.sq)
 }
 
